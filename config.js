0186e22beb0444ba5e13ff1fe48be8ac367442de
Rizwan Nizarudin <56226566+StuntStorm@users.noreply.github.com>
2021-06-19 16:18:36 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 
 module.exports = {
     //CHANGE ALL OF THESE
-    channelid: "852446471769030687",
+    channelid: "XXXX",
 
 
     botname: "StuntStorm",
 
     // API AUTHORIZATION
-    authorization: "NTk3MzAxOTQ1MDczNjY0MDEw.MTYyMzgyMjc4NjIwOQ==.e92654992c1e922a294a5decbe178542"
+    authorization: "XXXX"
 }
